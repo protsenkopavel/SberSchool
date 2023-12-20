29db28dbb3db6cf5fdb2a8a753e24f2c7cef6a4c
Pavel Protsenko <pavelprotsenko@outlook.com>
2023-12-20 19:58:12 +0300

    hw is done

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Child child1 = new Child();
+        Child child2 = new Child();
+    }
+}
---
+++ src/Parent.java
@@ -0,0 +1,28 @@
+abstract class Parent {
+    private String name;
+
+    static {
+        System.out.println("Parent:static 1");
+    }
+
+    {
+        System.out.println("Parent:instance 1");
+    }
+
+    static {
+        System.out.println("Parent:static 2");
+    }
+
+    public Parent() {
+        System.out.println("Parent:constructor");
+    }
+
+    {
+        System.out.println("Parent:instance 2");
+    }
+
+    public Parent(String name) {
+        this.name = name;
+        System.out.println("Parent:name-constructor");
+    }
+}

cdfeac23e597dbc45620c367894fd68bccc3aff5
Pavel Protsenko <pavelprotsenko@outlook.com>
2023-12-28 20:39:01 +0300

    Hm 3 is done

--- src/Lesson3/CollectionUtils.java
+++ src/Lesson3/CollectionUtils.java
@@ -1,9 +1,11 @@
 package Lesson3;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class CollectionUtils {
+public class CollectionUtils<T> {
     public static <T> void addAll(List<? extends T> source, List<? super T> destination) {
         destination.addAll(source);
     }
@@ -17,7 +19,7 @@ public class CollectionUtils {
     }
 
     public static <T> List<T> limit(List<? extends T> source, int size) {
-
+        return source.stream().limit(size).collect(Collectors.toList());
     }
 
     public static <T> void add(List<? super T> source, T t) {
@@ -41,11 +43,23 @@ public class CollectionUtils {
         return false;
     }
 
-    public static <T> List<T> range(List<? extends T> list, T min, T max) {
-
+    public static <T extends Comparable<? super T>> List<T> range(List<? extends T> list, T min, T max) {
+        List<T> result = new ArrayList<>();
+        for (T item : list) {
+            if (item.compareTo(min) >= 0 && item.compareTo(max) <= 0) {
+                result.add(item);
+            }
+        }
+        return result;
     }
 
     public static <T> List<T> range(List<? extends T> list, T min, T max, Comparator<? super T> comparator) {
-
+        List<T> result = new ArrayList<>();
+        for (T item : list) {
+            if (comparator.compare(item, min) >= 0 && comparator.compare(item, max) <= 0) {
+                result.add(item);
+            }
+        }
+        return result;
     }
 }
